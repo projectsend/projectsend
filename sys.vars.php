8bd011242f221d8b305d33838f428fa4798c093e
Ignacio Nelson <info@subwaydesign.com.ar>
2015-06-08 02:03:02 -0300

    Updated the language files / release

--- sys.vars.php
+++ sys.vars.php
@@ -14,7 +14,7 @@
  * Current version.
  * Updated only when releasing a new downloadable complete version.
  */
-define('CURRENT_VERSION', 'r572');
+define('CURRENT_VERSION', 'r582');
 
 /**
  * Fix for including external files when on HTTPS.

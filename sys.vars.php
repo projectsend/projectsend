648c91eb9813735663f059fdc816f94fc2f70e06
Ignacio Nelson <info@subwaydesign.com.ar>
2016-09-01 03:46:14 -0300

    Changed the version number

--- sys.vars.php
+++ sys.vars.php
@@ -14,7 +14,7 @@
  * Current version.
  * Updated only when releasing a new downloadable complete version.
  */
-define('CURRENT_VERSION', 'r611');
+define('CURRENT_VERSION', 'r638');
 
 /**
  * Fix for including external files when on HTTPS.

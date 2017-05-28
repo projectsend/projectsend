d5811781d13715a56a7dc743eeb783fc21063be9
Ignacio Nelson <info@subwaydesign.com.ar>
2017-05-28 04:44:11 -0300

    Redesigned the donations message
    Shameless, I know!

--- includes/updates.messages.php
+++ includes/updates.messages.php
@@ -13,15 +13,21 @@
 		if ($updates_made > 0) {
 ?>
 			<div id="system_msg">
-				<p><strong><?php _e('System Notice:', 'cftp_admin');?></strong> <?php _e('The database was updated to support this version of the software.', 'cftp_admin'); ?></p>
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-sm-12">
+							<p><strong><?php _e('System Notice:', 'cftp_admin');?></strong> <?php _e('The database was updated to support this version of the software.', 'cftp_admin'); ?></p>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-sm-12">
-						<div class="message message_info" id="donations_message">
-								<h3><strong><?php _e('Do you want to support ProjectSend?', 'cftp_admin');?></strong></h3>
-								<p><?php _e('Please remember that this tool is free software. If you find the system useful', 'cftp_admin'); ?>
-								<a href="<?php echo DONATIONS_URL; ?>" target="_blank"><?php _e('please consider making a donation to support further development.', 'cftp_admin'); ?></a>
+			<div id="donations_message">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-sm-12">
+							<h3><strong><?php _e('Do you want to support ProjectSend?', 'cftp_admin');?></strong></h3>
+							<p><?php _e('Please remember that this tool is free software. If you find the system useful', 'cftp_admin'); ?>
+								<a href="<?php echo DONATIONS_URL; ?>" target="_blank"><?php _e('please consider making a donation to support further development.', 'cftp_admin'); ?></a><br>
 								<?php _e('Thank you!', 'cftp_admin'); ?>
 							</p>
 						</div>

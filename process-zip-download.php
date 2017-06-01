c86b939211137b6485c31f62b2318fb1bda78d6d
Ignacio Nelson <info@subwaydesign.com.ar>
2017-06-01 15:22:03 -0300

    Zip download fixes
    1- Solution by remez on issue 410 to download large files by using
    stream_get_contents like regular files
    2- Fix for admins not being able to download complete zip files
    3- When downloading zipped files, add each individual file to the
    download count even for system users

--- process-zip-download.php
+++ process-zip-download.php
@@ -45,24 +45,24 @@ foreach ($files_to_zip as $file_to_zip) {
 	 * If the file is being generated for a client, make sure
 	 * that only files under his account can be added.
 	 */
-	if ($current_level == 0) {
-		$statement = $dbh->prepare("SELECT id, url, original_url, expires, expiry_date FROM " . TABLE_FILES . " WHERE id = :file");
-		$statement->bindParam(':file', $file_to_zip, PDO::PARAM_INT);
-		$statement->execute();
-		$statement->setFetchMode(PDO::FETCH_ASSOC);
-		$row = $statement->fetch();
+	$statement = $dbh->prepare("SELECT id, url, original_url, expires, expiry_date FROM " . TABLE_FILES . " WHERE id = :file");
+	$statement->bindParam(':file', $file_to_zip, PDO::PARAM_INT);
+	$statement->execute();
+	$statement->setFetchMode(PDO::FETCH_ASSOC);
+	$row = $statement->fetch();
 
-		$this_file_id			= $row['id'];
-		$this_file_on_disk		= $row['url'];
-		$this_file_save_as		= (!empty( $row['original_url'] ) ) ? $row['original_url'] : $row['url'];
-		$this_file_expires		= $row['expires'];
-		$this_file_expiry_date	= $row['expiry_date'];
+	$this_file_id			= $row['id'];
+	$this_file_on_disk		= $row['url'];
+	$this_file_save_as		= (!empty( $row['original_url'] ) ) ? $row['original_url'] : $row['url'];
+	$this_file_expires		= $row['expires'];
+	$this_file_expiry_date	= $row['expiry_date'];
 
-		$this_file_expired		= false;
-		if ($this_file_expires == '1' && time() > strtotime($this_file_expiry_date)) {
-			$this_file_expired	= true;
-		}
+	$this_file_expired		= false;
+	if ($this_file_expires == '1' && time() > strtotime($this_file_expiry_date)) {
+		$this_file_expired	= true;
+	}
 		
+	if ($current_level == 0) {
 		if ($this_file_expires == '0' || $this_file_expired == false) {
 			$statement = $dbh->prepare("SELECT * FROM " . TABLE_FILES_RELATIONS . " WHERE (client_id = :client_id OR FIND_IN_SET(group_id, :groups)) AND file_id = :file_id AND hidden = '0'");
 			$statement->bindParam(':client_id', $global_id, PDO::PARAM_INT);
@@ -78,15 +78,6 @@ foreach ($files_to_zip as $file_to_zip) {
 														'on_disk'	=> $this_file_on_disk,
 														'save_as'	=> $this_file_save_as
 													);
-	
-				/** Add the download row */
-				$statement = $dbh->prepare("INSERT INTO " . TABLE_DOWNLOADS . " (user_id , file_id, remote_ip, remote_host)"
-											." VALUES (:user_id, :file_id, :remote_ip, :remote_host)");
-				$statement->bindValue(':user_id', CURRENT_USER_ID, PDO::PARAM_INT);
-				$statement->bindParam(':file_id', $this_file_id, PDO::PARAM_INT);
-				$statement->bindParam(':remote_ip', $_SERVER['REMOTE_ADDR']);
-				$statement->bindParam(':remote_host', $_SERVER['REMOTE_HOST']);
-				$statement->execute();
 			}
 		}
 	}
@@ -97,6 +88,15 @@ foreach ($files_to_zip as $file_to_zip) {
 							);
 	}
 
+
+	/** Add the download row */
+	$statement = $dbh->prepare("INSERT INTO " . TABLE_DOWNLOADS . " (user_id , file_id, remote_ip, remote_host)"
+								." VALUES (:user_id, :file_id, :remote_ip, :remote_host)");
+	$statement->bindValue(':user_id', CURRENT_USER_ID, PDO::PARAM_INT);
+	$statement->bindParam(':file_id', $this_file_id, PDO::PARAM_INT);
+	$statement->bindParam(':remote_ip', $_SERVER['REMOTE_ADDR']);
+	$statement->bindParam(':remote_host', $_SERVER['REMOTE_HOST']);
+	$statement->execute();
 }
 
 //echo $zip_file;print_r($allowed_to_zip); die();
@@ -120,13 +120,24 @@ if ($added_files > 0) {
 	$new_record_action = $new_log_action->log_action_save($log_action_args);
 
 	if (file_exists($zip_file)) {
-		$zip_file_name = 'download_files_'.generateRandomString().'.zip';
-		header('Content-Type: application/zip');
-		header('Content-Length: ' . filesize($zip_file));
+		$zip_file_name = 'projectsend_'.generateRandomString().'.zip';
+		session_write_close(); 
+		while (ob_get_level()) ob_end_clean();
+		header('Content-Type: application/octet-stream');
 		header('Content-Disposition: attachment; filename="'.$zip_file_name.'"');
-		ob_clean();
-		flush();
-		readfile($zip_file);
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+		header('Content-Length: ' . get_real_size($zip_file));
+		header('Pragma: public');
+		header('Cache-Control: private',false);
+		header('Connection: close');
+		$context = stream_context_create();
+		$file = fopen($zip_file, 'rb', false, $context);
+		while ( !feof( $file ) ) {
+		//usleep(1000000); //Reduce download speed
+			echo stream_get_contents($file, 2014);
+		}
+		fclose( $file );
 		unlink($zip_file);
 	}
 }

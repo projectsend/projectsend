ddda57188c5970b4b6b649c13b761a1101d99868
Ignacio Nelson <info@subwaydesign.com.ar>
2018-06-22 15:41:26 -0300

    Warning if simplexml_load_file module is not enable for news

--- app/templates-admin/widgets/news.php
+++ app/templates-admin/widgets/news.php
@@ -1,6 +1,6 @@
 <?php
 	if ( isset( $_GET['ajax_call'] ) ) {
-        require_once '../../../sys.includes.php';
+		require_once '../../../sys.includes.php';
 	}
 
 	$allowed_news = array(9,8,7);
@@ -9,27 +9,40 @@
 		<div class="widget">
 			<h4><?php _e('ProjectSend news','cftp_admin'); ?></h4>
 			<div class="widget_int">
-				<ul class="home_news">
-					<?php
-						$feed = simplexml_load_file(NEWS_FEED_URI);
-						$max_news = 3;
-						$n = 0;
-						foreach ($feed->channel->item as $item) {
-							if ($n < $max_news) {
-						?>
-								<li>
-									<span class="date"><?php echo date(TIMEFORMAT,strtotime($item->pubDate)); ?></span>
-									<a href="<?php echo html_output($item->link); ?>" target="_blank">
-										<h5><?php echo html_output($item->title); ?></h5>
-									</a>
-									<p><?php echo make_excerpt(html_output(strip_tags($item->description, '<br />')),200); ?>
-								</li>
-						<?php
-								$n++;
-							}
-						}
-					?>
-				</ul>
+				<?php
+					if ( function_exists( 'simplexml_load_file' ) ) {
+				?>
+						<ul class="home_news">
+							<?php
+								$feed = simplexml_load_file(NEWS_FEED_URI);
+								$max_news = 3;
+								$n = 0;
+								foreach ($feed->channel->item as $item) {
+									if ($n < $max_news) {
+								?>
+										<li>
+											<span class="date"><?php echo date(TIMEFORMAT,strtotime($item->pubDate)); ?></span>
+											<a href="<?php echo html_output($item->link); ?>" target="_blank">
+												<h5><?php echo html_output($item->title); ?></h5>
+											</a>
+											<p><?php echo make_excerpt(html_output(strip_tags($item->description, '<br />')),200); ?>
+										</li>
+								<?php
+										$n++;
+									}
+								}
+							?>
+						</ul>
+				<?php
+					}
+					else {
+				?>
+						<div class="alert alert-warning">
+							<?php echo sprintf(__('News cannot be loaded. Please enable the %s php module to fix this issue.', 'cftp_admin'), 'simplexml_load_file'); ?>
+						</div>
+				<?php
+					}
+				?>
 			</div>
 		</div>
 <?php

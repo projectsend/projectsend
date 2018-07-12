d5eb33a0f39917d61df1d7a8252f51732b1f5ed7
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2018-07-12 14:09:00 -0400

    Update sent.php

--- sent.php
+++ sent.php
@@ -1500,7 +1500,6 @@ if($_REQUEST['edit'] == 1){echo '<div class="alert alert-success"><a href="#" cl
 									$formatted_size = '-';
 
 								}
-                if((expires = 0) || (expires = 1 and expiry_date > '" . date('Y-m-d H:i:s')."')) {
 
 					?>
 
@@ -1829,7 +1828,6 @@ echo $data['user'].'</br>';
                 </tr>
 
                 <?php
-		                                             }
 
 							}
 

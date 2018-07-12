16bf2150b4412c6a151b6440f17035a9b3e23355
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2018-07-12 12:18:09 -0400

    Update sent.php

--- sent.php
+++ sent.php
@@ -1250,7 +1250,7 @@ include('header.php');
 
             <section id="no-more-tables">
 <?php
-if($_REQUEST['edit'] == 1){echo '<div class="alert alert-success"><a href="#" class="close" data-dismiss="alert">×</a>The file has been edited succesfuly.</div>';}
+if($_REQUEST['edit'] == 1){echo '<div class="alert alert-success"><a href="#" class="close" data-dismiss="alert">×</a>The file has been edited successfully.</div>';}
 ?>
             <table id="files_list" class="cc-mail-listing-style table table-striped table-bordered table-hover dataTable no-footer" data-page-size="<?php echo FOOTABLE_PAGING_NUMBER; ?>">
 

8dec1cb85305a5e4758cba89d9b925ae2bc7d524
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2018-07-13 11:13:14 -0400

    Update outbox.php

--- outbox.php
+++ outbox.php
@@ -1520,7 +1520,7 @@ include('header.php');
                                             if($current_level != '0') {
 
                                                 $download_link = BASE_URI.'process.php?do=download&amp;client='.$global_user.'&amp;id='.$row['id'].'&amp;n=1';
-                                                if (($row['expires'] != '0') && (time() < strtotime($row['expiry_date']))) {
+                                                if (($row['expires'] == '0') || (time() < strtotime($row['expiry_date']))) {
                                         ?>
 
                                                 <a href="<?php echo $download_link; ?>" target="_blank">

bc542366aa1859519c72ecd56901bc6ebbb3e2ad
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2018-07-17 15:15:17 -0400

    Update upload-import-orphans.php

--- upload-import-orphans.php
+++ upload-import-orphans.php
@@ -181,9 +181,9 @@ $work_folder = UPLOADED_FILES_FOLDER;
                 <tr>
                   <th class="td_checkbox" data-sort-ignore="true"> <input type="checkbox" name="select_all" id="select_all" value="0" />
                   </th>
-                  <th data-sort-initial="true"><?php _e('File name','cftp_admin'); ?></th>
-                  <th data-type="numeric" data-hide="phone"><?php _e('File size','cftp_admin'); ?></th>
-                  <th data-type="numeric" data-hide="phone"><?php _e('Last modified','cftp_admin'); ?></th>
+                  <th data-sort-initial="true"><?php _e('File Name','cftp_admin'); ?></th>
+                  <th data-type="numeric" data-hide="phone"><?php _e('File Size','cftp_admin'); ?></th>
+                  <th data-type="numeric" data-hide="phone"><?php _e('Last Modified','cftp_admin'); ?></th>
                   
                 </tr>
               </thead>
@@ -322,3 +322,67 @@ $work_folder = UPLOADED_FILES_FOLDER;
 <?php
 	include('footer.php');
 ?>
+
+
+
+<style type="text/css">
+/*-------------------- Responsive table by B) -----------------------*/
+@media only screen and (max-width: 1200px) {
+    #content {
+        padding-top:30px;
+    }
+    
+    /* Force table to not be like tables anymore */
+    #no-more-tables table, 
+    #no-more-tables thead, 
+    #no-more-tables tbody, 
+    #no-more-tables th, 
+    #no-more-tables td, 
+    #no-more-tables tr { 
+        display: block; 
+    }
+ 
+    /* Hide table headers (but not display: none;, for accessibility) */
+    #no-more-tables thead tr { 
+        position: absolute;
+        top: -9999px;
+        left: -9999px;
+    }
+ 
+    #no-more-tables tr { border: 1px solid #ccc; }
+ 
+    #no-more-tables td { 
+        /* Behave  like a "row" */
+        border: none;
+        border-bottom: 1px solid #eee; 
+        position: relative;
+        padding-left: 50%; 
+        white-space: normal;
+        text-align:left;
+    }
+ 
+    #no-more-tables td:before { 
+        /* Now like a table header */
+        position: absolute;
+        /* Top/left values mimic padding */
+        top: 6px;
+        left: 6px;
+        width: 45%; 
+        padding-right: 10px; 
+        white-space: nowrap;
+        text-align:left;
+        font-weight: bold;
+    }
+ 
+    /*
+    Label the data
+    */
+
+    
+    td:nth-of-type(1):before { content: ""; }
+    td:nth-of-type(2):before { content: "File Name"; }
+    td:nth-of-type(3):before { content: "File Size"; }
+    td:nth-of-type(4):before { content: "Last Modified"; }
+}
+/*-------------------- Responsive table End--------------------------*/
+</style>

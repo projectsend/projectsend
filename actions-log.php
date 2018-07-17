8abe98944031ac49da900acfb39a3515a78bd1b4
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2018-07-17 15:28:12 -0400

    Update actions-log.php

--- actions-log.php
+++ actions-log.php
@@ -284,3 +284,68 @@ include('header.php');
 
 <?php include('footer.php'); ?>
 
+
+<style type="text/css">
+/*-------------------- Responsive table by B) -----------------------*/
+@media only screen and (max-width: 1200px) {
+    #content {
+        padding-top:30px;
+    }
+    
+    /* Force table to not be like tables anymore */
+    #no-more-tables table, 
+    #no-more-tables thead, 
+    #no-more-tables tbody, 
+    #no-more-tables th, 
+    #no-more-tables td, 
+    #no-more-tables tr { 
+        display: block; 
+    }
+ 
+    /* Hide table headers (but not display: none;, for accessibility) */
+    #no-more-tables thead tr { 
+        position: absolute;
+        top: -9999px;
+        left: -9999px;
+    }
+ 
+    #no-more-tables tr { border: 1px solid #ccc; }
+ 
+    #no-more-tables td { 
+        /* Behave  like a "row" */
+        border: none;
+        border-bottom: 1px solid #eee; 
+        position: relative;
+        padding-left: 50%; 
+        white-space: normal;
+        text-align:left;
+    }
+ 
+    #no-more-tables td:before { 
+        /* Now like a table header */
+        position: absolute;
+        /* Top/left values mimic padding */
+        top: 6px;
+        left: 6px;
+        width: 45%; 
+        padding-right: 10px; 
+        white-space: nowrap;
+        text-align:left;
+        font-weight: bold;
+    }
+ 
+    /*
+    Label the data
+    */
+
+    
+    td:nth-of-type(1):before { content: ""; }
+    td:nth-of-type(2):before { content: "Date"; }
+    td:nth-of-type(3):before { content: "Author"; }
+    td:nth-of-type(4):before { content: "Activity"; }
+    td:nth-of-type(5):before { content: ""; }
+    td:nth-of-type(6):before { content: ""; }
+    td:nth-of-type(7):before { content: ""; }
+}
+/*-------------------- Responsive table End--------------------------*/
+</style>

f09593c4f005f732bc3bb3c31427ddc5d4106a41
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2018-07-17 16:01:33 -0400

    Update userorganization.php

--- userorganization.php
+++ userorganization.php
@@ -270,14 +270,14 @@ include('header.php');
                     echo system_message('error',$no_results_message);
                 }
             ?>
-    <section id="no-more-tables">
+    <section id="no-more-tables" class="cc-overflow-scroll">
             <table id="groups_tbl" class="table table-striped table-bordered table-hover dataTable no-footer" data-page-size="<?php echo FOOTABLE_PAGING_NUMBER; ?>">
                 <thead>
                     <tr>
                         <th class="td_checkbox" data-sort-ignore="true">
                             <input type="checkbox" name="select_all" id="select_all" value="0" />
                         </th>
-                        <th data-sort-initial="true"><?php _e('Organization name','cftp_admin'); ?></th>
+                        <th data-sort-initial="true"><?php _e('Organization Name','cftp_admin'); ?></th>
                         <th data-hide="phone"><?php _e('Description','cftp_admin'); ?></th>
                         <th data-type="numeric"><?php _e('Members','cftp_admin'); ?></th>
                         <th data-hide="phone" data-type="numeric"><?php _e('Files','cftp_admin'); ?></th>
@@ -351,3 +351,69 @@ include('header.php');
     </div>
 </div>
 <?php include('footer.php'); ?>
+
+
+<style type="text/css">
+/*-------------------- Responsive table by B) -----------------------*/
+@media only screen and (max-width: 1200px) {
+    #content {
+        padding-top:30px;
+    }
+    
+    /* Force table to not be like tables anymore */
+    #no-more-tables table, 
+    #no-more-tables thead, 
+    #no-more-tables tbody, 
+    #no-more-tables th, 
+    #no-more-tables td, 
+    #no-more-tables tr { 
+        display: block; 
+    }
+ 
+    /* Hide table headers (but not display: none;, for accessibility) */
+    #no-more-tables thead tr { 
+        position: absolute;
+        top: -9999px;
+        left: -9999px;
+    }
+ 
+    #no-more-tables tr { border: 1px solid #ccc; }
+ 
+    #no-more-tables td { 
+        /* Behave  like a "row" */
+        border: none;
+        border-bottom: 1px solid #eee; 
+        position: relative;
+        padding-left: 50%; 
+        white-space: normal;
+        text-align:left;
+    }
+ 
+    #no-more-tables td:before { 
+        /* Now like a table header */
+        position: absolute;
+        /* Top/left values mimic padding */
+        top: 6px;
+        left: 6px;
+        width: 45%; 
+        padding-right: 10px; 
+        white-space: nowrap;
+        text-align:left;
+        font-weight: bold;
+    }
+ 
+    /*
+    Label the data
+    */
+
+    
+    td:nth-of-type(1):before { content: ""; }
+    td:nth-of-type(2):before { content: "Organization Name"; }
+    td:nth-of-type(3):before { content: "Description"; }
+    td:nth-of-type(4):before { content: "Members Files"; }
+    td:nth-of-type(5):before { content: "Created by"; }
+    td:nth-of-type(6):before { content: "Added on"; }
+    td:nth-of-type(10):before { content: "Actions"; }
+}
+/*-------------------- Responsive table End--------------------------*/
+</style>

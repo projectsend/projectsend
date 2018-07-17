d2ecb965d80e633165692a03459435c8e494b33c
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2018-07-17 15:02:25 -0400

    Update categories.php

--- categories.php
+++ categories.php
@@ -292,7 +292,7 @@ include('header.php');
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-xs-12 col-sm-12 col-md-12">
-                    <section id="no-more-tables">
+                    <section id="no-more-tables" class="cc-overflow-scroll">
 						<table id="categories_tbl" class="table table-striped table-bordered table-hover dataTable no-footer" data-page-size="<?php echo FOOTABLE_PAGING_NUMBER; ?>">
 							<thead>
 								<tr>
@@ -400,3 +400,67 @@ include('header.php');
 </div>
 
 <?php include('footer.php'); ?>
+
+
+<style type="text/css">
+/*-------------------- Responsive table by B) -----------------------*/
+@media only screen and (max-width: 1200px) {
+    #content {
+        padding-top:30px;
+    }
+    
+    /* Force table to not be like tables anymore */
+    #no-more-tables table, 
+    #no-more-tables thead, 
+    #no-more-tables tbody, 
+    #no-more-tables th, 
+    #no-more-tables td, 
+    #no-more-tables tr { 
+        display: block; 
+    }
+ 
+    /* Hide table headers (but not display: none;, for accessibility) */
+    #no-more-tables thead tr { 
+        position: absolute;
+        top: -9999px;
+        left: -9999px;
+    }
+ 
+    #no-more-tables tr { border: 1px solid #ccc; }
+ 
+    #no-more-tables td { 
+        /* Behave  like a "row" */
+        border: none;
+        border-bottom: 1px solid #eee; 
+        position: relative;
+        padding-left: 50%; 
+        white-space: normal;
+        text-align:left;
+    }
+ 
+    #no-more-tables td:before { 
+        /* Now like a table header */
+        position: absolute;
+        /* Top/left values mimic padding */
+        top: 6px;
+        left: 6px;
+        width: 45%; 
+        padding-right: 10px; 
+        white-space: nowrap;
+        text-align:left;
+        font-weight: bold;
+    }
+ 
+    /*
+    Label the data
+    */
+
+    
+    td:nth-of-type(1):before { content: ""; }
+    td:nth-of-type(2):before { content: "Name"; }
+    td:nth-of-type(3):before { content: "Files"; }
+    td:nth-of-type(4):before { content: "Description"; }
+    td:nth-of-type(5):before { content: "Actions"; }
+}
+/*-------------------- Responsive table End--------------------------*/
+</style>

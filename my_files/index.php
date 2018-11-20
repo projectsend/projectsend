c6b2470a60a8cdc8625065d494e37ae83c3999b1
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2018-11-20 10:14:15 -0500

    Update index.php

--- my_files/index.php
+++ my_files/index.php
@@ -2,7 +2,7 @@
  <?php
 	require_once('../sys.includes.php');
 	$this_user = CURRENT_USER_USERNAME;
-	if (!empty($_GET['client']) && CURRENT_USER_LEVEL != '0') {
+	if (!empty($_GET['client']) && CURRENT_USER_LEVEL == '0') {
 		$this_user = $_GET['client'];
 	}
 	$url = SITE_URI."inbox.php";

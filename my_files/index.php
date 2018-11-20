f9a56b36b4a1dbb485c180f9d09082c65b128d06
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2018-11-20 13:09:12 -0500

    Update index.php

--- my_files/index.php
+++ my_files/index.php
@@ -2,13 +2,13 @@
  <?php
 	require_once('../sys.includes.php');
 	$this_user = CURRENT_USER_USERNAME;
-	if (!empty($_GET['client']) && CURRENT_USER_LEVEL == '0') {
+	if (!empty($_GET['client']) && CURRENT_USER_LEVEL != '0') {
 		$this_user = $_GET['client'];
 	}
-	// $url = SITE_URI."inbox.php";
+	$url = SITE_URI."inbox.php";
 	if(CURRENT_USER_LEVEL !='9') {
 		echo "<script>";
-		echo "window.location ='https://send.microhealthllc.com/inbox.php'";
+		echo "window.location.href ='".$url."'";
 		echo "</script>";
 	}
 	else {

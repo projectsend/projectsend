edb313912da10ec9c435726b6149a48682fe701d
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2019-02-22 13:35:10 -0500

    Update clients-form.php

--- clients-form.php
+++ clients-form.php
@@ -225,6 +225,7 @@ switch ($clients_form_type) {
 	?>
 
 	<div class="inside_form_buttons cc-text-right">
+                <a href="<?php echo BASE_URI; ?>clients.php" name="cancel" class="btn btn-wide btn-default"><?php _e('Cancel','cftp_admin'); ?></a>
 		<button type="submit" name="submit" class="btn btn-wide btn-primary"><?php echo html_output($submit_value); ?></button>
 	</div>
 

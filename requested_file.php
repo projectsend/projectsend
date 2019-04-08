3fe0ad5c0657f9fb8d1b1616f2e329094e0b9eb1
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2019-04-08 16:30:40 -0400

    Update requested_file.php

--- requested_file.php
+++ requested_file.php
@@ -222,7 +222,7 @@ $actual_link = SITE_URI.'requested_file.php';
                         <span class="checkmark"></span> </label>
                     </th>
                     <th data-type="numeric" data-sort-initial="descending" data-hide="phone"><?php _e('To name','cftp_admin'); ?></th>
-                    <th data-hide="phone,tablet"><?php _e('Subject.','cftp_admin'); ?></th>
+                    <th data-hide="phone,tablet"><?php _e('Subject','cftp_admin'); ?></th>
                     <th data-hide="phone,tablet"><?php _e('Organization','cftp_admin'); ?></th>
 
                     <th><?php _e('email','cftp_admin'); ?></th>
@@ -304,7 +304,7 @@ $actual_link = SITE_URI.'requested_file.php';
                                             <span class="checkmark"></span> </label>
                                     </th>
                                     <th data-type="numeric" data-sort-initial="descending" data-hide="phone"><?php _e('From name','cftp_admin'); ?></th>
-                                    <th data-hide="phone,tablet"><?php _e('Subject.','cftp_admin'); ?></th>
+                                    <th data-hide="phone,tablet"><?php _e('Subject','cftp_admin'); ?></th>
                                     <th data-hide="phone,tablet"><?php _e('Organization','cftp_admin'); ?></th>
 
                                     <th><?php _e('email','cftp_admin'); ?></th>
@@ -330,7 +330,7 @@ $actual_link = SITE_URI.'requested_file.php';
                                                 <span class="checkmark"></span> </label></td>
                                         <td><?php echo $row['from_name']; ?></td>
                                         <td class="file_name"><?php echo $row['to_subject_request']; ?></td>
-                                        <td><?php echo $row['from_organization']; ?></td>
+                                        <td></td>
                                         <td><?php echo $row['from_email']; ?></td>
                                         <td><?php echo $row['to_note_request']; ?></td>
                                         <td class="<?php echo (!empty($row['hidden'])) ? 'file_status_hidden' : 'file_status_visible'; ?>">

08173f938d380ef64eb20d28c9060cece9e3b280
Your Name <you@example.com>
2019-06-07 11:39:19 +0530

    Issue 552 Guest delete

--- includes/classes/actions-files.php
+++ includes/classes/actions-files.php
@@ -67,45 +67,22 @@ class FilesActions
 	}
 	function delete_inbox_files($rel_id)
 	{
-		$this->can_delete		= false;
 		$this->result			= '';
 		$this->check_level		= array(9,8,0);
 
 		if (isset($rel_id)) {
 			/** Do a permissions check */
 			if (isset($this->check_level) && in_session_or_cookies($this->check_level)) {
-				$this->file_id = $rel_id;
-				$this->sql = $this->dbh->prepare("SELECT url, uploader FROM " . TABLE_FILES . " WHERE id = :file_id");
-				$this->sql->bindParam(':file_id', $this->file_id, PDO::PARAM_INT);
-				$this->sql->execute();
-				$this->sql->setFetchMode(PDO::FETCH_ASSOC);
-				while( $this->row = $this->sql->fetch() ) {
-					// if ( CURRENT_USER_LEVEL == '0' ) {
-					// 	if ( CLIENTS_CAN_DELETE_OWN_FILES == '1' && $this->row['uploader'] == CURRENT_USER_USERNAME ) {
-					// 		$this->can_delete	= true;
-					// 	}
-					// }
-					// else {
-					// 	$this->can_delete	= true;
-					// }
-					$this->can_delete	= true;
-
-					$this->file_url = $this->row['url'];
-				}
 
 				/** Delete the reference to the file on the database */
-				if ( true === $this->can_delete ) {
 					$this->sql = $this->dbh->prepare("DELETE FROM " . TABLE_FILES_RELATIONS. " WHERE file_id = :file_id AND client_id =".CURRENT_USER_ID);
-					$this->sql->bindParam(':file_id', $this->file_id, PDO::PARAM_INT);
+					$this->sql->bindParam(':file_id', $rel_id, PDO::PARAM_INT);
 					$this->sql->execute();
-					$prev_assign = $this->dbh->prepare("UPDATE " . TABLE_FILES . " SET prev_assign ='1' WHERE id = :file_id");
-					$prev_assign->bindParam(':file_id', $this->file_id, PDO::PARAM_INT);
+					$prev_assign = $this->dbh->prepare("DELETE FROM " . TABLE_FILES . " WHERE id = :file_id");
+					$prev_assign->bindParam(':file_id', $rel_id, PDO::PARAM_INT);
 					$prev_assign->execute();
 					$this->result = true;
-				}
-				else {
-					$this->result = false;
-				}
+
 
 				return $this->result;
 			}

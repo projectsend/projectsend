bbe0e4c1dcbfd3bef298903dc9a17c520e27002a
Renjith Pillai <you@example.com>
2019-08-22 15:30:58 +0530

    Encryption for zip-files added

--- aesencrypt.php
+++ aesencrypt.php
@@ -66,5 +66,35 @@ class AESENCRYPT {
 
       return $error ? false : $dest;
   }
+  function decryptZipFile($source)
+  {
+      $key = "8765416198765416187654161987654161";
+      $key = substr(sha1($key, true), 0, 16);
+      $dest = 'upload/files/temp/zip/'.$source;
+      $source = 'upload/files/temp/'.$source;
+      $fileEncryptionblocks = 10000000;
+      $error = false;
+      if ($fpOut = fopen($dest, 'w')) {
+          if ($fpIn = fopen($source, 'rb')) {
+              // Get the initialzation vector from the beginning of the file
+              $iv = fread($fpIn, 16);
+              while (!feof($fpIn)) {
+                  $ciphertext = fread($fpIn, 16 * ($fileEncryptionblocks + 1)); // we have to read one block more for decrypting than for encrypting
+                  $plaintext = openssl_decrypt($ciphertext, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
+                  // Use the first 16 bytes of the ciphertext as the next initialization vector
+                  $iv = substr($ciphertext, 0, 16);
+                  fwrite($fpOut, $plaintext);
+              }
+              fclose($fpIn);
+          } else {
+              $error = true;
+          }
+          fclose($fpOut);
+      } else {
+          $error = true;
+      }
+
+      return $error ? false : $dest;
+  }
 }
  ?>

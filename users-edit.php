2d090447a1207a1da4e0b32afdfd667906452c0f
Renjith Pillai <you@example.com>
2019-09-16 12:09:01 +0530

    #540 Profile pic in my account

--- users-edit.php
+++ users-edit.php
@@ -177,9 +177,13 @@ include('header.php');
 			
                 <div class="col-sm-3 profile-pic"> 
 				<?php
-
+				if (empty($_GET['id'])){
+					$pic_id = $user_id ;
+				} else {
+					$pic_id = $_GET['id'] ; 
+				}
 				$profile_pic = $dbh->prepare("SELECT * FROM " . TABLE_USER_EXTRA_PROFILE . " WHERE user_id=:user_id AND name = :name");
-				$profile_pic->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+				$profile_pic->bindParam(':user_id', $pic_id, PDO::PARAM_INT);
 				$profile_pic->bindValue(':name', 'profile_pic');
 				$profile_pic->execute();
 				$profile_pic->setFetchMode(PDO::FETCH_ASSOC);

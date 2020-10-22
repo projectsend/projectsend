fa7da6df04dc5f26e41c8b9ce1a8ec9161feb6dc
Ignacio Nelson <info@subwaydesign.com.ar>
2020-10-21 21:38:57 -0300

    Enable CSRF

--- includes/security/csrf.php
+++ includes/security/csrf.php
@@ -24,7 +24,7 @@ function validateCsrfToken()
     return isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $_REQUEST['csrf_token']);
 }
 
-// if (!defined('IS_INSTALL') && $_POST && !validateCsrfToken()) {
-//     header("Location: ".PAGE_STATUS_CODE_403);
-//     exit;
-// }
+if (!defined('IS_INSTALL') && $_POST && !validateCsrfToken()) {
+    header("Location: ".PAGE_STATUS_CODE_403);
+    exit;
+}

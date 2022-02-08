28fe22b0d5b9816ccf1f30383ed108ffe5e4851b
Ignacio Nelson <info@subwaydesign.com.ar>
2022-02-08 09:16:00 -0300

    Fixed: dashboard counters labels where not translatable

--- includes/widgets/counters.php
+++ includes/widgets/counters.php
@@ -29,27 +29,27 @@
                 <ul>
                     <li>
                         <h6><?php echo $total_files; ?></h6>
-                        <h5>Files</h5>
+                        <h5><?php _e('Files','cftp_admin'); ?></h5>
                         <i class="fa fa-file"></i>
                     </li>
                     <li>
                         <h6><?php echo $total_downloads; ?></h6>
-                        <h5>Downloads</h5>
+                        <h5><?php _e('Downloads','cftp_admin'); ?></h5>
                         <i class="fa fa-download"></i>
                     </li>
                     <li>
                         <h6><?php echo $total_clients; ?></h6>
-                        <h5>Clients</h5>
+                        <h5><?php _e('Clients','cftp_admin'); ?></h5>
                         <i class="fa fa-address-card"></i>
                     </li>
                     <li>
                         <h6><?php echo $total_groups; ?></h6>
-                        <h5>Groups</h5>
+                        <h5><?php _e('Groups','cftp_admin'); ?></h5>
                         <i class="fa fa-th-large"></i>
                     </li>
                     <li>
                         <h6><?php echo $total_users; ?></h6>
-                        <h5>System Users</h5>
+                        <h5><?php _e('System Users','cftp_admin'); ?></h5>
                         <i class="fa fa-users"></i>
                     </li>
                 </ul>

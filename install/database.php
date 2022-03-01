868ddc387a5100c516b6cf9f9eafe21e2c0d8a88
Ignacio Nelson <info@subwaydesign.com.ar>
2022-03-01 13:49:33 -0300

    Enable csv uploading by default

--- install/database.php
+++ install/database.php
@@ -247,7 +247,7 @@ if (defined('TRY_INSTALL')) {
 								('logo_thumbnails_folder', '/assets/img/custom/thumbs'),
 								('timezone', 'America/Argentina/Buenos_Aires'),
 								('timeformat', 'd/m/Y'),
-								('allowed_file_types', '7z,ace,ai,avi,bin,bmp,bz2,cdr,doc,docm,docx,eps,fla,flv,gif,gz,gzip,htm,html,iso,jpeg,jpg,mp3,mp4,mpg,odt,oog,ppt,pptx,pptm,pps,ppsx,pdf,png,psd,rar,rtf,tar,tif,tiff,tgz,txt,wav,xls,xlsm,xlsx,xz,zip'),
+								('allowed_file_types', '7z,ace,ai,avi,bin,bmp,bz2,cdr,csv,doc,docm,docx,eps,fla,flv,gif,gz,gzip,htm,html,iso,jpeg,jpg,mp3,mp4,mpg,odt,oog,ppt,pptx,pptm,pps,ppsx,pdf,png,psd,rar,rtf,tar,tif,tiff,tgz,txt,wav,xls,xlsm,xlsx,xz,zip'),
 								('logo_filename', ''),
 								('admin_email_address', :email),
 								('clients_can_register', '0'),

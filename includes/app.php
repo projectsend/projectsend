dc0e2968cc74138102956096c6064676adc15227
Ignacio Nelson <info@subwaydesign.com.ar>
2022-05-25 22:45:37 -0300

    Updated translations / version bump

--- includes/app.php
+++ includes/app.php
@@ -47,7 +47,7 @@ define('DONATIONS_URL','https://www.projectsend.org/donations/');
  * Current version.
  * Updated only when releasing a new downloadable complete version.
  */
-define('CURRENT_VERSION', 'r1380');
+define('CURRENT_VERSION', 'r1405');
 
 /**
  * Required software versions

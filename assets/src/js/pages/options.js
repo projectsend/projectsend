5dd86186e71a2baf2f4a0771c4f3afbb2b80a4b5
Ignacio Nelson <info@subwaydesign.com.ar>
2022-07-06 19:30:04 -0300

    Updated @yaireo/tagify

--- assets/src/js/pages/options.js
+++ assets/src/js/pages/options.js
@@ -2,14 +2,15 @@
     'use strict';
 
     admin.pages.options = function () {
+        var tagifyContainer = document.getElementById('allowed_file_types');
+        var tagify = new Tagify (tagifyContainer);
+        //tagifyContainer.addEventListener('change', tagifyOnChange)
+
+        function tagifyOnChange(e){
+            console.log(e.target.value)
+        }
 
         $(document).ready(function(){
-            $('#allowed_file_types')
-            .tagify()
-            .on('add', function(e, tagName){
-                console.log('added', tagName)
-            });
-
             var validator = $("#options").validate({
                 errorPlacement: function(error, element) {
                     error.appendTo(element.parent('div'));

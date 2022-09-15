9933e4c0ca2695917eb5667f4163f67f40c9ee7f
Ignacio Nelson <info@subwaydesign.com.ar>
2022-09-15 02:33:38 -0300

    Update functions.actionslog.php

--- includes/functions.actionslog.php
+++ includes/functions.actionslog.php
@@ -376,31 +376,31 @@ function format_action_log_record($params)
         case 50:
             $part1 = $owner_user;
             $action_text = __('Created an asset','cftp_admin');
-            $formatted = sprintf(__('%s created the asset "%s" (%s)','cftp_admin'), $owner_user, $details->title, $details->type);
+            $formatted = sprintf(__('%s created the asset "%s" (%s)','cftp_admin'), $owner_user, $details->title, $details->language);
             $type = 'system';
         break;
         case 51:
             $part1 = $owner_user;
             $action_text = __('Edited an asset','cftp_admin');
-            $formatted = sprintf(__('%s edited the asset "%s" (%s)','cftp_admin'), $owner_user, $details->title, $details->type);
+            $formatted = sprintf(__('%s edited the asset "%s" (%s)','cftp_admin'), $owner_user, $details->title, $details->language);
             $type = 'system';
         break;
         case 52:
             $part1 = $owner_user;
             $action_text = __('Deleted an asset','cftp_admin');
-            $formatted = sprintf(__('%s deleted the asset "%s" (%s)','cftp_admin'), $owner_user, $details->title, $details->type);
+            $formatted = sprintf(__('%s deleted the asset "%s" (%s)','cftp_admin'), $owner_user, $details->title, $details->language);
             $type = 'system';
         break;
         case 53:
             $part1 = $owner_user;
             $action_text = __('Enabled an asset','cftp_admin');
-            $formatted = sprintf(__('%s enabled the asset "%s" (%s)','cftp_admin'), $owner_user, $details->title, $details->type);
+            $formatted = sprintf(__('%s enabled the asset "%s" (%s)','cftp_admin'), $owner_user, $details->title, $details->language);
             $type = 'system';
         break;
         case 54:
             $part1 = $owner_user;
             $action_text = __('Disabled an asset','cftp_admin');
-            $formatted = sprintf(__('%s disabled the asset "%s" (%s)','cftp_admin'), $owner_user, $details->title, $details->type);
+            $formatted = sprintf(__('%s disabled the asset "%s" (%s)','cftp_admin'), $owner_user, $details->title, $details->language);
             $type = 'system';
         break;
     }

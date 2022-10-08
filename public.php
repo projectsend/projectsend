e18ece164789b12d80fb2d7b6c0602b731a31683
Ignacio Nelson <info@subwaydesign.com.ar>
2022-10-08 17:17:10 -0300

    Fixed permissions

--- public.php
+++ public.php
@@ -20,12 +20,16 @@ if (get_option('public_listing_logged_only') == 1) {
 $mode = 'files';
 
 // If viewing a particular group, make sure it's public
-if (!empty($_GET['token']) && !empty($_GET['group'])) {
-    $mode = 'group';
+if (!empty($_GET['group'])) {
+    if (empty($_GET['token'])) {
+        ps_redirect(BASE_URI . "index.php");
+    }
 
     if (!can_view_public_group($_GET['group'], $_GET['token'])) {
         ps_redirect(BASE_URI . "index.php");
     }
+
+    $mode = 'group';
     $current_group = new \ProjectSend\Classes\Groups($_GET['group']);
     $group_props = $current_group->getProperties();
 }
